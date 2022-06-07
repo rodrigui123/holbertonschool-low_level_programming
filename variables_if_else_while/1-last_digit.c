c7c1d544ed87a6b08ba38e5eac3d7d9d6884a034
Rodrigo <5093@holbertonschool.com>
2022-06-07 10:25:23 -0700

    p2t1C

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,14 +13,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	digit = n % 10;
+	digit = (n % 10);
 	printf("Last digit of %d is %d", n, digit);
 	if (digit > 5)
-		printf("%d and is greater than 5\n", n);
+		printf("and is greater than 5\n");
 	else if (digit == 0)
-		printf("%d and is 0\n", n);
+		printf("and is 0\n");
 	else
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
 
