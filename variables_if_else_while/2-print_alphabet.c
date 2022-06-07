1acc97f23a3f3cd0205648530a02520db45a177b
Rodrigo <5093@holbertonschool.com>
2022-06-07 13:10:50 -0700

    t2C

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main()
+{
+	char r;
+
+	for(int r = 'a'; r <= 'z'; r++)
+		putchar(r);
+	
+	return (0);
+}
