585a8800bef04d70f81bd3b29dbfbca3bb5ebc06
Rodrigo <5093@holbertonschool.com>
2022-06-07 14:28:16 -0700

    t8C

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char r;
+
+	for (r = '0'; r <= '9'; r++)
+	{
+	putchar(r);
+	}
+	for (r = 'a'; r <= 'f'; r++)
+	{
+	putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
+
