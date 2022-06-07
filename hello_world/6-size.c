c351d9332a08d884f7989b448c402a4a26ee3f2f
Rodrigo <5093@holbertonschool.com>
2022-06-06 19:54:26 -0700

    t6C

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("%dSize of a char\n", sizeof (char));
+	printf("%dSize of an int\n", sizeof (int));
+	printf("%dsize of a long int\n", sizeof (long int));
+	printf("%dSize of a long long int\n", sizeof (long long int));
+	printf("%dSize of a float\n", sizeof (float));
+
+	return (0);
+}
