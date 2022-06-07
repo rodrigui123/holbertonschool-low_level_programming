10ee092c16609454e9a0e87a68c6410bf553be8a
Rodrigo <5093@holbertonschool.com>
2022-06-07 14:09:47 -0700

    t7C

---
+++ variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r <= 'a'; r++)
+	{
+	putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
