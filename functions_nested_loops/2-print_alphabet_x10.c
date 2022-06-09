3f78bf61a53f7c0d633bc321d45e2381fc0e227a
Rodrigo <5093@holbertonschool.com>
2022-06-09 10:50:16 -0700

    t2_putch

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@ void print_alphabet_x10(void)
 {
 	char l;
 	int n;
-	for (n=0; n<10; n++) 
+	
+	for (n = 0; n < 10; n++) 
 	{
-	for (l= 'a'; l <= 'z'; l++)
+	for (l = 'a'; l <= 'z'; l++)
 	_putchar(l);
 	_putchar('\n');
 	}
