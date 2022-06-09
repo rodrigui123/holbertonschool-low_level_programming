6b9d2a76f831e648e196643d5ef494597e6422ca
Rodrigo <5093@holbertonschool.com>
2022-06-09 11:25:10 -0700

    t3putch

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - print_alphabet will print the whole alphabet
+*
+* Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+	if ( c > 'a' && c <= 'z' )
+	       return ( 1 );
+	{
+	return ( 0 );
+	}
+}	
