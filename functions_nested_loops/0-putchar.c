6ceb8ac2f4369889009d4053fa68cfd69b8c1899
Rodrigo <5093@holbertonschool.com>
2022-06-09 06:45:09 -0700

    t0putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	ptchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+
