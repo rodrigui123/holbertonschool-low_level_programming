f75f9e8564003cf12a2427ae3ac27c35c34096fc
Rodrigo <5093@holbertonschool.com>
2022-06-09 09:50:24 -0700

    t1putch

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+	putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
