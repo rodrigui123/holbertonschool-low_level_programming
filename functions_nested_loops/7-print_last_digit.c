fa90f0f6c44c06f85e8d0caab9181d6203bb1888
Rodrigo <5093@holbertonschool.com>
2022-06-10 09:42:09 -0700

    t7lstdigit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_last_digit - prints last digit of d
+ * @d : number that will change
  * Return: Always 0.
  */
-int print_last_digit (int d)
+int print_last_digit(int d)
 {
 	int lastDigit = d % 10;
-	if (d !=0);
-	{
-	_putchar(d%10, d);
-	}	
-}	
+
+	if (d < 0)
+		d = -d;
+	_putchar(lastDigit);
+}
