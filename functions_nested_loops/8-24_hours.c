c5e32d1a4afd44660e3e79b1212b78a4155cab0e
Rodrigo <5093@holbertonschool.com>
2022-06-10 13:23:05 -0700

    t8minshrs

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - printing his day mins ans hrs
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int mins;
+
+	for (hours = 0; hours <= 23; hours++)
+	for (mins = 0; mins <= 59; mins++)
+	{
+		_putchar(hours / 10) + '0';
+		_putchar(hours % 10);
+		_putchar(':');
+		_putchar(mins / 10);
+		_putchar(mins % 10);
+	}
+		_putchar('\n');
+}
