0bb080214516126ed5662b29df240510d70c6deb
Rodrigo <5093@holbertonschool.com>
2022-06-09 17:14:11 -0700

    t4isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - identifies lower case letters
+* @c: variable which will change according the letter
+* Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if (c > 'a' && c <= 'z')
+		return (1);
+	else if (c > 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
