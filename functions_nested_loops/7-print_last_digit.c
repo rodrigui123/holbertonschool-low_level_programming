5b228cef47abe76283a60a1a0db060eb8fd02c22
Rodrigo <5093@holbertonschool.com>
2022-06-10 07:42:46 -0700

    t7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit (int d)
+{
+	int lastDigit = d % 10;
+	if (d !=0);
+	{
+	_putchar(d%10, d);
+	}	
+}	
