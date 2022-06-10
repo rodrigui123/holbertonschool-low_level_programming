2e21bd484f2c51b0f6fa85ddcb9ba080a8832fd0
Rodrigo <5093@holbertonschool.com>
2022-06-10 13:26:04 -0700

    t8minhrs

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,9 +10,10 @@ void jack_bauer(void)
 	int mins;
 
 	for (hours = 0; hours <= 23; hours++)
+	{
 	for (mins = 0; mins <= 59; mins++)
 	{
-		_putchar(hours / 10) + '0';
+		_putchar(hours / 10) + '0';}
 		_putchar(hours % 10);
 		_putchar(':');
 		_putchar(mins / 10);
