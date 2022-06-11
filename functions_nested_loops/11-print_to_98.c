b2cacf7a54fa7fa3d29c8ab6570e0a4f412f955a
Rodrigo <5093@holbertonschool.com>
2022-06-11 16:15:06 -0700

    t11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,30 @@
  */
 void print_to_98(int n)
 {
+	if (n < 98);
+	{
 	for (; n < 98; n++)
 	{
 		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
 	}
+	}
+	if (n > 98);
+	{
 	for (; n > 98; n--)
 	{
 		_putchar((n) + 48);
 		_putchar(',');
 		_putchar(' ');
 	}
+	}
+	if (n = 98);
+	{
 	for (; n == 98;)
 	{
 		_putchar((n) + 48);
+	}
 	}
 	_putchar (10);
 }
