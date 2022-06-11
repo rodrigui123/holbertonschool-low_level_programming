9fb531327b7ab3decd98190b198ce62213f5d383
Rodrigo <5093@holbertonschool.com>
2022-06-11 15:05:01 -0700

    t10add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds the integer
+ * @a : first integer
+ * @b : second integer
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
