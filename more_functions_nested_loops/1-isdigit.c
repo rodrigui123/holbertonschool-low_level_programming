874bd664f228fec09e6d2603df53b562b09e871c
Rodrigo <5093@holbertonschool.com>
2022-06-12 15:40:30 -0700

    t1isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,13 @@
 
 /**
  * _isdigit - the digit that may vary
+ * @c : variable of numbers
  * Return: Always 0.
  */
 int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
-		return (1);
-	else
 		return (0);
+	else
+		return (1);
 }
