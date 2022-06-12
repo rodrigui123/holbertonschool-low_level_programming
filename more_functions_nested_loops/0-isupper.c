ee516c38ff4e57db20e0dace071fb928b6d3ef19
Rodrigo <5093@holbertonschool.com>
2022-06-12 15:16:08 -0700

    t0moref

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
+ * _isupper - checks uppercase 
+ * @c : the numbers
  * Return: Always 0.
  */
 int _isupper(int c)
 {
-	if (c > 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	return (1);
 	else
 		return (0);
