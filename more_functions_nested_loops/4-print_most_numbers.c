c1da6db59ba52b91d6b6782384ee87b76499fcdd
Rodrigo <5093@holbertonschool.com>
2022-06-12 17:33:01 -0700

    t4mostnumb

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers asked
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	if ((n != '2') && (n != '4'))
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+	}
+}
