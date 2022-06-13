ce4f3113edff36162816238b967dbe805840a411
Rodrigo <5093@holbertonschool.com>
2022-06-12 18:47:32 -0700

    t5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,10 @@ void more_numbers(void)
 		{
 			if (n > '9')
 			{
-				_putchar(n);
-				_putchar((n % 10) + '0');
+				_putchar((n / 10) + '0');
 			}
-		 		_putchar('\n');
+			_putchar((n % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
