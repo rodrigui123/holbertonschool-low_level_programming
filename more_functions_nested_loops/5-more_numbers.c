76280f1ceac3ee715663cc8ef4975aec8e769561
Rodrigo <5093@holbertonschool.com>
2022-06-12 18:13:18 -0700

    t5morenum

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,14 @@
  */
 void more_numbers(void)
 {
+	int r;
 	int n;
 
 		for (n = 0; n < 10; n++)
 		{
 		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n);
+			_putchar((n) + 48);
 			_putchar('\n');
 		}
 		}
