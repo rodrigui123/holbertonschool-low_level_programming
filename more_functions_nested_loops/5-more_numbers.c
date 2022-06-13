7138b8f371d992853ac645dcd29801a4f17ffca5
Rodrigo <5093@holbertonschool.com>
2022-06-12 17:55:51 -0700

    t5mnumb

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - numbers to print
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int r;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	for (r = 0; r <= 14; r++)
+	{
+		_putchar(r);
+		_putchar('\n');
+	}
+	}
+}
