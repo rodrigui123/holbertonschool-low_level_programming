948954c055c7f39814659b1a2f3d605d728a0aa6
Rodrigo <5093@holbertonschool.com>
2022-06-12 17:05:03 -0700

    t3numb

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers asked
+ * @n : variable of numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	
+		_putchar(n);
+	_putchar('\n');
+	
+}
