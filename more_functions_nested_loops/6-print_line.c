df3f46e89d29ec8c0b0d761e49baff8472a5ac54
Rodrigo <5093@holbertonschool.com>
2022-06-12 19:00:10 -0700

    t6prntline

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_line - draws a straight line
+* @n: number of times char printed
+*/
+void print_line(int n)
+{
+	for (; n > 0; n--)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+}
