c6d0b0a88a8855a6ff6e87879a71f4cd8386b8ae
Rodrigo <5093@holbertonschool.com>
2022-06-14 11:10:47 -0700

    t1swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,14 +3,13 @@
 
 /**
  * swap_int - swap values of a b
- * @num : variable to store the value of a 
+ * @r : variable to store the value of a 
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
 {
-	int num;
-
-	num = *a
+	int r;
+	r = *a
 	*a = *b
-	num = *b
+	r = *b
 }
