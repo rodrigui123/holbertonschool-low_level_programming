efef9155d8791c0c3d8546d87cbbe6153988e08e
Rodrigo <5093@holbertonschool.com>
2022-06-15 12:13:02 -0700

    t5rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i;
+	int a;
+	int b;
+	char str[] = "";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	b = i - 1;
+	for (a = 0; a < b; a++)
+	{
+		str[0] = s[b];
+		s[b] = s[a];
+		s[a] = str[0];
+		b--;
+	}
+}
