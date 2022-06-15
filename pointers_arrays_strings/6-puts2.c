074f5dc05a2510a00101b9dfc6d632cdc116b24b
Rodrigo <5093@holbertonschool.com>
2022-06-15 14:09:56 -0700

    t6puts

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 /**
  * puts2 - prints every other character of a string
- *
+ * @str : variable that prints %2
  * Return: Always 0.
  */
 void puts2(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	
+
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
-}	
+}
