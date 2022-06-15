2f7690fee2a74e2dd2eceeac9c5ce34ac627d5f8
Rodrigo <5093@holbertonschool.com>
2022-06-15 14:08:28 -0700

    t6puts

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,14 +11,11 @@ void puts2(char *str)
 	
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	}
+	
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
-		else
-		{
-			continue;
-		}
+	}
 	_putchar('\n');
 }	
