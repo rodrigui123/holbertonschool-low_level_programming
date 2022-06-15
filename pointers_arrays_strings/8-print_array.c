492210803d018541479ba716e3b6c2c78f988979
Rodrigo <5093@holbertonschool.com>
2022-06-15 16:33:09 -0700

    t8array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 /**
  * print_array - print elements of an array of integers
- * @a : 
- *
+ * @a : array
+ * @n : last number of array
  * Return: Always 0.
  */
 void print_array(int *a, int n)
 {
 	int len;
-	
+
 	for (len = 0; len < n; len++)
 	{
 		if (len == n - 1)
@@ -17,6 +17,6 @@ void print_array(int *a, int n)
 			printf("%d\n", a[len]);
 		}
 		else
-			printf("%d", a[len]);
+			printf("%d, ", a[len]);
 	}
 }
