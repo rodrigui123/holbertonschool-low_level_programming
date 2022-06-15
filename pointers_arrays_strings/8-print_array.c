98ca97c1bb7342f548709948d987de536ae0c3bb
Rodrigo <5093@holbertonschool.com>
2022-06-15 16:15:16 -0700

    t8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int len;
+
+	for (len = 0; len < n; len++);
+	{
+		if (len = n - 1);
+		{
+			_putchar(a[len]);
+		}
+		_putchar(a[len]);
+		_putchar(',');
+		_putchar(' ');
+	}
+}		
