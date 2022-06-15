135fc4c27af9b6efddb3e63ac5dff5a21c6b2777
Rodrigo <5093@holbertonschool.com>
2022-06-15 08:30:53 -0700

    t4rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,15 @@
  * Return: Always 0.
  */
 void print_rev(char *s)
-{
-	int i, c = 0;
-	
-	printf(s);
-	gets(*s);
+{ 
+	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	c++;
 	}
-	for (i = c-1; i >=0; i--)
-	{	
-		printf("%c", s[i]);
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]); 	
 	}
 	_putchar('\n');
-	return (0);
 }
