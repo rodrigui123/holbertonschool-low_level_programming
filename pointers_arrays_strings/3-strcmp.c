8ea82e5076b26c934b6a2b028274b0d7f8dc8e35
Rodrigo <5093@holbertonschool.com>
2022-06-17 12:43:56 -0700

    t3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return(s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
+}
