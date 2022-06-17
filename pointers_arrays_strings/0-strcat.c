5488e64dd05f9ba507b33c5d8a08302201d0cae3
Rodrigo <5093@holbertonschool.com>
2022-06-16 17:02:29 -0700

    t0morep

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+"main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lenscr; lendest;
+
+	for (lendest = 0; dest[lendest]; lendest++)
+	{
+	}
+	for (lenscr = 0; src[lenscr]; lenscr++)
+	{
+		src[lenscr] = dest[lendest];
+		lendest++;
+	}
+	dest[lendest] = 0;
+	return (dest);
+}	
