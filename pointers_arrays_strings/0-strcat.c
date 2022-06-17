d2308de3d94611157fd3a78313c966146cd546e7
Rodrigo <5093@holbertonschool.com>
2022-06-16 17:13:46 -0700

    t0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * strcat - appends src at the end of dest
+ * @dest : destination string
+ * @src : source string
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
-	int lenscr; 
+	int lenscr;
 	int lendest;
 
 	for (lendest = 0; dest[lendest]; lendest++)
@@ -21,4 +22,4 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[lendest] = 0;
 	return (dest);
-}	
+}
