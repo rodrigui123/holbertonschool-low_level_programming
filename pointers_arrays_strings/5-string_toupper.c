ce1177e6678527964519f870ebf7150cae25f014
Rodrigo <5093@holbertonschool.com>
2022-06-17 14:04:14 -0700

    t5stringto

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	i++;
+	}
+	return (0);
+}	
