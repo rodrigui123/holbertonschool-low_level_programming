37d8d3fd6eea6b818e4929087528c28388200566
Rodrigo <5093@holbertonschool.com>
2022-06-17 13:21:20 -0700

    t4rev

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
