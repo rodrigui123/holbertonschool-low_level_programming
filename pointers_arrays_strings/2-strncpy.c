8442b677806847bceebfdf915291e082a8eefa91
Rodrigo <5093@holbertonschool.com>
2022-06-17 11:25:05 -0700

    t2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
