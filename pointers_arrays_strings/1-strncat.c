c7ba706f8a406ad45eba3515ffbc4e563ab5af82
Rodrigo <5093@holbertonschool.com>
2022-06-17 08:40:37 -0700

    t1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int lendest;
+	int lensrc;
+
+	for (lensrc = 0; lensrc < n && src [lensrc] != '\0'; lensrc++)
+	{
+		dest[lendest + lensrc] = src[lensrc];
+	}
+	dest[lendest + lensrc] = '\0';
+	return (dest);
+}
