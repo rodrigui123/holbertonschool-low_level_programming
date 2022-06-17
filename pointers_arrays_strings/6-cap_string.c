ccdd4fb38c93fffc79ae09bc7f64dea87b0b115a
Rodrigo <5093@holbertonschool.com>
2022-06-17 14:53:43 -0700

    t6capstring

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		else if (s[i] >= 97 && s[i] <= 122)
+		{
+			if (s[i - 1] = ',' || s[i -1] = ';' || s[i - 1] = '.' || s[i - 1] = '!' || s[i -1] = '?' || s[i - 1] = '"' || s[i - 1] = '(' || s[i - 1] = ')' || s[i - 1] = '{' || s[i - 1] = '}')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+	}
+	return (s[i]);
+} 
