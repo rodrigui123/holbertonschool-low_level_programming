2644cf90318642c73d05bed634ea8ca01831b70d
Rodrigo <5093@holbertonschool.com>
2022-06-20 11:18:59 -0700

    t1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,10 +9,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '0')
 		return;
-	if (*s < 0)
-	{
-		_putchar(*s);
-		s--;
-		_print_rev_recursion(s);
-	}
+	
+	_print_rev_recursion(s + 1);
+		_putchar(s);
 }
