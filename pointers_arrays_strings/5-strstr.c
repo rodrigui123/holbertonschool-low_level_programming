ad7a0af6d7db8a796801c3ed54987fd5cb2716bf
Rodrigo <5093@holbertonschool.com>
2022-06-19 19:50:03 -0700

    t5strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -5,21 +5,21 @@
  * _strstr - function that locates a substring
  * @haystack : main string
  * @needle : other string
- * Return: Always 0.
+ * Return: a pointer to the beginning of the located substring, or NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j;
 
-	for (i = 0; haystack[i]; i++)
+for (i = 0; haystack[i]; i++)
 	{
 		for (j = 0; needle[j]; j++)
 		{
 			if (needle[j] != haystack[i + j])
 				break;
 		}
-		if (needle[i] == '\0')
+		if (needle[j] == '\0')
 			return (&haystack[i]);
 	}
 	return (NULL);
