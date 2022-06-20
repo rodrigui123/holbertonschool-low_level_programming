4b363c4f8a7de64c051f2d592e5ac9676e322161
Rodrigo <5093@holbertonschool.com>
2022-06-20 13:41:47 -0700

    t4pow

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _pow_recursion - raises the value of x to the power of y
+ * @x : value to be raised
+ * @y : the value to raise x
  * Return: Always 0.
  */
 int _pow_recursion(int x, int y)
