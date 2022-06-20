6d622daf5e2455f1cf9936f106030ebf56f8b59b
Rodrigo <5093@holbertonschool.com>
2022-06-19 17:56:55 -0700

    t6chess

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	
---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int row;
+	int column;
+
+	for (i; i < 8; i++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[i][column]);
+		}
+		_putchar('\n');
+}		
