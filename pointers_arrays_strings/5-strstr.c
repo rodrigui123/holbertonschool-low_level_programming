73f5c91c389b60e1d71f18802c036cb990cab123
Rodrigo <5093@holbertonschool.com>
2022-06-19 18:52:07 -0700

    t5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 		for (j = 0; needle[j]; j++)
 		{
 			if (haystack[i] == needle[j])
-				return (haystack[0]);
+				return (haystack[i];
 			if (haystack[i] != needle[j])
 				return (NULL);
 		}
