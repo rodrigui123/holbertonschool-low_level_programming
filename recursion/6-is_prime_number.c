40fbfd5075a321574ca6a38e1de9cfe04dd19102
Rodrigo <5093@holbertonschool.com>
2022-06-20 17:15:41 -0700

    t6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prim - function that looks for a prime number
+ * @n : number to see if its prime
+ * @i : variable
+ * Return: Always 0.
+ */
+int prim(int n, int i)
+{
+	if (i == n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	prim(n, i + 1);
+}
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - function that tests if number is prime
+ * @n : the number to test
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	return (prim(n, 2));
+}
