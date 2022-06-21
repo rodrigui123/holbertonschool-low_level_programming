93e17513020d8a93a89f5a757372820edb5f4366
Rodrigo <5093@holbertonschool.com>
2022-06-21 12:19:26 -0700

    t1args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,8 +5,8 @@
 * @argv : an array of size argc
 * Return: 0.
 */
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", (argc - 1));
+	printf("%d\n", (argc - 1);
 	return (0);
 }
