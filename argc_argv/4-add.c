ca5ae2fbc9b06744f49ef095a38df28322a4cb50
Rodrigo <5093@holbertonschool.com>
2022-06-21 16:14:44 -0700

    t4add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main -  entry point
+* @argc : int
+* @argv : an array of size argc
+* Return: 0.
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; argv[i]; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if(isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; argv[i]; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}	
