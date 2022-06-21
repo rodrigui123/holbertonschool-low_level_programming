12bf44f929276a5653311db138e20e79613bbe19
Rodrigo <5093@holbertonschool.com>
2022-06-21 13:00:21 -0700

    t2args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -10,8 +10,8 @@ int main(__attribute__((unused)) int argc, char *argv[])
 	int i;
 
 	for (i = 0; argv[i]; i++)
-	{	
-		printf ("%s\n",argv[i]);
+	{
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
