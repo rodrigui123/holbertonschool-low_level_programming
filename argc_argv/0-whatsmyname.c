62385402cda2981b70a794d923d0877cee775c20
Rodrigo <5093@holbertonschool.com>
2022-06-21 12:14:46 -0700

    t0whatsmyn

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 /**
-* main - 
-* @argc : 
-* @argv :
+* main -  entry poiny
+* @argc : the number command line arguments  
+* @argv : an array of size argc
 * Return: 0.
 */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
+	printf("%d\n", (argc - 1));
 	return (0);
 }
