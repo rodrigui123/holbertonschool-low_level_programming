4dbc1b1da73daecacdbcdb5ad0ddadb8c50603e3
Rodrigo <5093@holbertonschool.com>
2022-06-21 12:29:53 -0700

    t1args

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main -  entry poiny
+* @argc : the number command line arguments 
+* @argv : an array of size argc
+* Return: 0.
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
