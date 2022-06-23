64b8d1659e24afb1074789dfc4baa36ae2f3a831
Rodrigo <5093@holbertonschool.com>
2022-06-23 12:36:54 -0700

    t0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	a = malloc( size * sizeof(char));
+	if (a = '\0')
+		return (NULL);
+	for (i = 0; i <= size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
