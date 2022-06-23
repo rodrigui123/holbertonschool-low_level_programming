e9251e9ca35b900f88e1da90f2e17eaa40e0dfef
Rodrigo <5093@holbertonschool.com>
2022-06-23 12:48:06 -0700

    t0createarray

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,6 +6,7 @@
  * create_array - function that creates an array of chars
  * @c : char that has to be in every position of the array
  * Return: the inicial position of the array
+ * @size : size of the array
  */
 char *create_array(unsigned int size, char c)
 {
@@ -14,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	a = malloc( size * sizeof(char));
+	a = malloc(size * sizeof(char));
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i <= size; i++)
