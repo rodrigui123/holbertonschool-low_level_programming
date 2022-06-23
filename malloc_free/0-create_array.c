c700459b7e565dd2aa01b1d7d9512c7c35f53259
Rodrigo <5093@holbertonschool.com>
2022-06-23 12:39:28 -0700

    t0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	a = malloc( size * sizeof(char));
-	if (a = '\0')
+	if (a == NULL)
 		return (NULL);
 	for (i = 0; i <= size; i++)
 	{
