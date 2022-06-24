3cb7112851e7c2081c2cc69edb7366368a2af003
Rodrigo <5093@holbertonschool.com>
2022-06-24 12:01:48 -0700

    t3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -26,11 +26,11 @@ int **alloc_grid(int width, int height)
 			{
 				free(ar[i]);
 			}
-			free(ar);
-			return (NULL);
+		free(ar);
+		return (NULL);
 			for (j = 0; j < width; j++)
 			{
-				ar[i][j] = 0;	
+				ar[i][j] = 0;
 			}
 		}
 	}
