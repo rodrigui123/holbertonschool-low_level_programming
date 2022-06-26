c1f688fffef306194de59a248333f2ac2ced0e61
Rodrigo <5093@holbertonschool.com>
2022-06-26 16:24:43 -0700

    t1stringnconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, x, size1;
 	char *arr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	size1 = strlen(s1);
 	i = 0;
 	if (n >= strlen(s2))
