854b6d82f277417d7dcb8e0e04c5fa5de315cacf
Rodrigo <5093@holbertonschool.com>
2022-06-26 18:39:08 -0700

    t2calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,4 +12,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void r;
 
+	if (size == 0)
+		return (NULL);
+	if (nmemb == 0)
+		return (NULL);
+	r = malloc(nmemb * size);
+	if (r == NULL)
+		return (NULL);
+	memset(r, 0, nmemb * size);
+	return (r);
+}
