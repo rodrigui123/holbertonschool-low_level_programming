b2d789fe734564fbb65386a2a836b45cb96d1e2a
Rodrigo <5093@holbertonschool.com>
2022-06-26 19:42:52 -0700

    t3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,28 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "main.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+* array_range - creates an array of integers (from min to max)
+* @min: min value
+* @max: max value
+* Return: pointer to new array
+*/
 int *array_range(int min, int max)
 {
-	int *arr, Lenar, i;
+	int *arr, arlen, i;
 
 	if (min > max)
 		return (NULL);
-	Lenar = max - min + 1;
-	arr = malloc(Lenar * sizeof(int));
+	arlen = max - min + 1;
+	arr = malloc(arlen * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; min <= max; i++,  min++);
-	{
-		arr[i] = min;
-	}
+	for (i = 0; min <= max; i++, min++)
+		arr[] = min;
 	return (arr);
 }
