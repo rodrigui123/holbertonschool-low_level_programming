f4c2f6c54b2d318d5cae8aa2e75b80d6e2fa7089
Rodrigo <5093@holbertonschool.com>
2022-06-26 19:46:50 -0700

    t3arrayrange

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,8 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	arlen = max - min + 1;
-	arr = malloc(arlen * sizeof(int));
-	
+	arr = malloc(arlen * sizeof(int));	
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; min <= max; i++, min++)
