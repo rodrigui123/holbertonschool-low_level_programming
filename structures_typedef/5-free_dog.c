e5186360ed508992b7d1d4effc22700c6a082b03
Rodrigo <5093@holbertonschool.com>
2022-06-29 14:20:45 -0700

    t5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	if (d->name == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	if (d->owner == NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+}
