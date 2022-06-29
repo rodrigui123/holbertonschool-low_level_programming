a5bab45084f352dee88164594083c937cc6e9399
Rodrigo <5093@holbertonschool.com>
2022-06-29 10:32:09 -0700

    t1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->*name = name;
+	d->age = age;
+	d->*owner = owner;
+}		   
