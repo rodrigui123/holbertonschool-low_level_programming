8da766e4765fb5219964d648787afff84405f83f
Rodrigo <5093@holbertonschool.com>
2022-06-29 14:22:28 -0700

    t5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -12,19 +12,16 @@ void free_dog(dog_t *d)
 	if (d == NULL)
 	{
 		free(d);
-		return (NULL);
 	}
 	if (d->name == NULL)
 	{
 		free(d->name);
 		free(d);
-		return (NULL);
 	}
 	if (d->owner == NULL)
 	{
 		free(d->owner);
 		free(d->name);
 		free(d);
-		return (NULL);
 	}
 }
