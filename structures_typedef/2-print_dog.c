75b06395285c171e8d304464a99d2b16547b0ab2
Rodrigo <5093@holbertonschool.com>
2022-06-29 11:10:11 -0700

    t2printdog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("%s\n", d->name ? d->name : "nil");
-	printf("%f\n", d->age);
-	printf("%s\n", d->owner ? d->owner : "nil");
+	printf("Name: %s\n", d->name ? d->name : "nil");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "nil");
 }
