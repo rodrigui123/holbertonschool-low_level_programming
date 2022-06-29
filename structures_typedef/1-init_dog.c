061c0ba0d4de6a2ced47620b0beb318c16d68499
Rodrigo <5093@holbertonschool.com>
2022-06-29 10:34:15 -0700

    t1initdog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,7 +8,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->*name = name;
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
 	d->age = age;
-	d->*owner = owner;
+	d->owner = owner;
 }		   
