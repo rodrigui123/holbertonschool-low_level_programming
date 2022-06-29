370f712467a9d95753340bb5f2af76ac259c12b4
Rodrigo <5093@holbertonschool.com>
2022-06-29 10:44:00 -0700

    t1initdog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,8 +2,11 @@
 #include "dog.h"
 
 /**
- * main - check the code
- *
+ * init_dog - initialize a variable of type struct dog
+ * @d : pointer to struct dog
+ * @name : component of dog structure
+ * @age : component of dog structure
+ * @owner : component of dog structure
  * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -13,4 +16,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-}		   
+}
