8c4cedc7992985381ced9fc84e030ceffa3d3df6
Rodrigo <5093@holbertonschool.com>
2022-06-29 14:27:23 -0700

    t5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,26 +3,26 @@
 #include <stdlib.h>
 
 /**
- * main - check the code
- *
+ * free_dog - frees the dogs
+ * @d : pointer to dogs
  * Return: Always 0.
  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
-	{
 		free(d);
-	}
-	if (d->name == NULL)
+	if (d)
 	{
-		free(d->name);
-		free(d);
+		if (d->name == NULL)
+		{
+			free(d->name);
+			free(d);
+		}
+		if (d->owner == NULL)
+		{
+			free(d->owner);
+			free(d->name);
+			free(d);
+		}
 	}
-	if (d->owner == NULL)
-	{
-		free(d->owner);
-		free(d->name);
-		free(d);
-	}
-	free(d->age);
 }
