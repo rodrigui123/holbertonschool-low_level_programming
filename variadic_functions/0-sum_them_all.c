95e139283b7b1e464c17dc34ad4e9d685c94de52
Rodrigo <5093@holbertonschool.com>
2022-07-01 09:43:56 -0700

    t0sumthemall

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -7,16 +7,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list b;
 	unsigned int i; 
 	int add = 0;
-	va_list b;
 
 	va_start(b, n);
 	if (n != 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			add += (b, int);
+			add += va_arg(b, int);
 		}
 	va_end(b);
 	return (add);
