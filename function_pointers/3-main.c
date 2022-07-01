18f2c1694dfa1b454d58bf5bca8951a3b39bbb76
Rodrigo <5093@holbertonschool.com>
2022-06-30 18:15:04 -0700

    t3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (r == NULL || strlen(argv[2]) != 1 )
+	if (r == NULL || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
