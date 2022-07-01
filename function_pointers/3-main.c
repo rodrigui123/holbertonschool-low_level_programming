78cf02fd0a2f6b2188b791f1e30a68ffa4cbbb62
Rodrigo <5093@holbertonschool.com>
2022-06-30 17:42:39 -0700

    t3main.c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+	int (*r)(int, int);
+	int j, p;
+
+	j = atoi(argv[1]);
+	p = atoi(argv[3]);
+	r = get_op_func(argv[2]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (r == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else
+		printf("%d\n", r(j, p));
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* op_add - function that sums a and b
+* @a : variable
+* @b : variable
+**/
+int op_add(int a, int b)
+{
+	return (a+b);
+}
+/**
+* op_sub - sub function 
+* @a : variable
+* @b : variable
+**/
+int op_sub(int a, int b)
+{
+	return (a-b);
+}
+/**
+* op_mul - function that multiplies
+* @a : variable
+* @b : variable
+**/
+int op_mul(int a, int b)
+{
+	return (a*b);
+}
+/**
+* op_div - divide function
+* @a : variable
+* @b : variable
+**/
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a/b);
+}
+/**
+* op_mod - mod function
+* @a : variable
+* @b : variable
+**/
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a%b);
+}
