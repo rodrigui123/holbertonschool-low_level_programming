dedd86d0e0516cef1dd666aa215d772f7a50dffb
Rodrigo <5093@holbertonschool.com>
2022-07-02 15:37:09 -0700

    t1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,3 +8,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 
+	va_list b;
+	unsigned int i;
+	
+	va_start(b, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i >= 1)
+			{
+				printf("%d", ", ");
+			}
+			printf("%d", va_arg(b, int);
+		}
+	}
+	va_end(b);
+	printf("%d\n");
+}
