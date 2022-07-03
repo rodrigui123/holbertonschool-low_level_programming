8a90d116a6acae292dd369a4116db592cde0cff1
Rodrigo <5093@holbertonschool.com>
2022-07-03 15:12:12 -0700

    t3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -31,9 +31,9 @@ void print_all(const char * const format, ...)
 					j = (va_arg(b, char *));
 					(j == NULL ? printf("(nil)") : printf("%s", j));
 					break;
-			}
-			if (i >= 1 && separator != NULL)
+			if (i >= 1)
 				printf("%s", ", ");
+			}
 			i++;
 		}
 	}
