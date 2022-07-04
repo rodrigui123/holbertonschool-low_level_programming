01187a8d7460a41f496e363106d8bdd48f079f98
Rodrigo <5093@holbertonschool.com>
2022-07-04 10:04:33 -0700

    t3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 	{
 		while (format[i])
 		{
-			if ((i >= 1) && (format[i] == 'c' || 
+			if (i >= 1 && (format[i] == 'c' ||
 			format[i]  == 'i' || format[i] == 'f' || format[i] == 's'))
 				printf(", ");
 			switch (format[i])
