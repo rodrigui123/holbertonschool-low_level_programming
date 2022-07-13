86405d7627ed8786814f17c8e78810c79ce980ab
Rodrigo <5093@holbertonschool.com>
2022-07-13 10:47:40 -0700

    t0printlist

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("%d %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return(count);
+}	
