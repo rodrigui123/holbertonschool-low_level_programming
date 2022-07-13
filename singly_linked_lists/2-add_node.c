daa9b86efd04f9f38c4af175815b04c59c048d33
Rodrigo <5093@holbertonschool.com>
2022-07-13 16:46:02 -0700

    t2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	struct *head new_node = (struct *head) malloc(sizeof(struct head));
+
+	new_node-> = str;
+	new_node->next = (headnode);
+	(headnode) = newnode;
+}
