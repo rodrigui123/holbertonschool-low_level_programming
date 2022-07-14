c0a09538714eae86b16cd4fa326b4c8b1252d0f6
Rodrigo <5093@holbertonschool.com>
2022-07-14 09:05:07 -0700

    t2addnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,15 +1,19 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add_node - add a node at the beginning of a list
+ * @newnode : the node that will be added at the beginning
+ * Return: newnode
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	struct *head new_node = (struct *head) malloc(sizeof(struct head));
+	newnode = malloc(sizeof(list_t));
 
-	new_node-> = str;
-	new_node->next = (headnode);
-	(headnode) = newnode;
+	if (newnode == NULL)
+		return (NULL);
+	newnode->str = strdup(str);
+	newnode->len = strlen(str);
+	newnode->next = *head;
+	*head = newnode->str;
+	return (newnode);
 }
