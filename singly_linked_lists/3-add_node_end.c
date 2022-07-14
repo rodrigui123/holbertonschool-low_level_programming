3248b46c8ea545a949b2bb21d0e9d3d5f842da01
Rodrigo <5093@holbertonschool.com>
2022-07-14 13:35:13 -0700

    t3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,16 +17,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	newnode->str = strdup(str);
 	newnode->len = strlen(str);
 	newnode->next = NULL;
-	if (*head == NULL)
+	if (newnode)
 	{
-		*head = newnode;
-		return(newnode);
+		if (*head == NULL)
+			*head = newnode;
+		while (lastdigit->next != NULL)
+			lastdigit = lastdigit->next;
+		lastdigit->next = newnode;
 	}
-	while (lastdigit->next != NULL)
-	{
-		lastdigit = lastdigit->next;
-	}
-	lastdigit->next = newnode;
-	
 	return (newnode);
 }
