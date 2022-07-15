6fb51484c31f22081e028f36d14e5a8cae64d26b
Rodrigo <5093@holbertonschool.com>
2022-07-15 10:38:43 -0700

    t1listintlen

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
+ * listint_len - function that returns the number of elements in a linked list
+ * @h : pointer to first position of list
  * Return: Always 0.
  */
 size_t listint_len(const listint_t *h)
