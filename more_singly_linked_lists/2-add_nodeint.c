74b1b3fe5888a706f6f6f5196f73c4a7e386d5d6
Rodrigo <5093@holbertonschool.com>
2022-07-15 12:26:33 -0700

    t2

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -12,8 +12,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
-	newnode->n = strdup(n);
-	newnnode->n = *head;
+	newnode->n = n;
+	newnnode->next = *head;
 	*head = newnode;
 
 	return (newnode);
