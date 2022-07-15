63721a151a4612bd6ce51b6d146d48b042620cb6
Rodrigo <5093@holbertonschool.com>
2022-07-15 14:01:45 -0700

    t3addnodeintend

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,6 +5,7 @@
  * lastdigit - last position to locate the new node
  * newnode - the node that will go at the end of list
  * @head : pointer of a pointer to list
+ * @n : content of the list (because they are numbers)
  * Return: Always 0.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
