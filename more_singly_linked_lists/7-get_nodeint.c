f6fdec5c0266334769af6e2fbf83dba490d28795
Rodrigo <5093@holbertonschool.com>
2022-07-15 17:47:27 -0700

    t7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -9,12 +9,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-	else
+	for (i = 0; i < index; i++)
 	{
-		for (i = 0; i < index; i++)
-			head = head->next;
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
 	}
 	return (head);
 }
