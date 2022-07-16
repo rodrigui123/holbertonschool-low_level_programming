ae747bd23908f1cfdb6f5742d5664d5206b036db
Rodrigo <5093@holbertonschool.com>
2022-07-15 17:53:05 -0700

    t7getnodeint

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - function that returns the nth node of a linked list
- * @head : pointer to first node of list 
- * @idex : index of nodes
+ * get_nodeint_at_index - function that returns the nth node of a linkedlist
+ * @head : pointer to first node of list
+ * @index : index of node
  * Return: head.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
