821c721b7a66b9b0a13c66f96e78b195c6605dd3
Rodrigo <5093@holbertonschool.com>
2022-07-15 17:45:47 -0700

    t7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	else
 	{
 		for (i = 0; i < index; i++)
-		{
-			head = head->next
-			
-		}
+			head = head->next;
 	}
 	return (head);
 }
