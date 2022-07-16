09ec4c90f24b538fe7a0a59dfa197af6a33fb5f5
Rodrigo <5093@holbertonschool.com>
2022-07-15 18:46:34 -0700

    t9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newnode, *aux = *head;
+	int i;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	while (i < (idx - 1) && aux)	
+	{
+		if (aux->next == NULL)
+			return (NULL);
+		i++;
+		aux = head->next;
+	}
+	newnode->next = aux->next;
+	aux->next = newnode;
+	return (newnode);
+}
