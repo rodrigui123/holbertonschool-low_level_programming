0cd068fb4e3a783a0d98933c5255aac21967b797
Rodrigo <5093@holbertonschool.com>
2022-07-15 17:45:08 -0700

    t7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * 
+ *
+ * Return: Always 0.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	else
+	{
+		for (i = 0; i < index; i++)
+		{
+			head = head->next
+			
+		}
+	}
+	return (head);
+}
