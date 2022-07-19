f2886e35d1ee912793e9ebbf1e537106e88478e0
Rodrigo <5093@holbertonschool.com>
2022-07-19 15:38:11 -0700

    t2getbit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n : number  
+ * @index : index
+ * Return: the rest of the index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	n = n >> index;
+	return (n % 2);
+}
