722f2c93e8cefbbb14759aa852494b3bb564aaa7
Rodrigo <5093@holbertonschool.com>
2022-07-20 16:44:36 -0700

    t1createfile

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL || fd == -1)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONGLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (text_content && text_content[count])
 		count++;
 	count = write(fd, text_content, count);
