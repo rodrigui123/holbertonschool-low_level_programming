b8608ed7121126bf9612788555dc5aea46ab5764
Rodrigo <5093@holbertonschool.com>
2022-07-20 17:26:14 -0700

    t2appendtext

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,9 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * append_text_to_file - appends text at the end of a file
+ * @filename : name of file
+ * @text_content : content of text in file
  * Return: Always 0.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+	close(fd);
+	return (1);
+
