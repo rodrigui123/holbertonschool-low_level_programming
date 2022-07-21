84568b3897897360aaf00c5347dc10a5cc5ff940
Rodrigo <5093@holbertonschool.com>
2022-07-20 19:28:54 -0700

    t3cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,12 +22,13 @@ int main(int argc, char *argv[])
 void copy_text(const char *from, const char *to)
 {
 	int file_from, file_to, readme = 1024, writeme;
-	char *Count[1024];
+	char Count[1024];
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	file_from = open(from, O_RDONLY);
-	if (file_from != -1)
+	if (file_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
+
 	file_to = open(to, O_CREAT | O_TRUNC | O_WRONLY, mode);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write %s\n", from), exit(99);
@@ -36,9 +37,9 @@ void copy_text(const char *from, const char *to)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
 	writeme = write(file_to, Count, readme);
 	if (writeme == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write %s\n", from), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write %s\n", to), exit(99);
 	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 }
