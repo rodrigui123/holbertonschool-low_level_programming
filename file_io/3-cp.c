1aa9d0d0fbbaef0da3bf0920d0a1fa5d3f466606
Rodrigo <5093@holbertonschool.com>
2022-07-20 19:10:26 -0700

    t3cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	copy_text(argv[1], argv[2]);
+	copy_txt(argv[1], argv[2]);
 	exit(0);
 }
 /**
@@ -23,15 +23,14 @@ void copy_txt(const char *from, const char *to)
 {
 	int file_from, file_to, readme = 1024, writeme;
 	char *Count[1024];
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_TROTH;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	file_from = open(from, 0_RDONLY);
+	file_from = open(from, O_RDONLY);
 	if (file_from != -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
 	file_to = open(to, O_CREAT | O_TRUNC | O_WRONLY, mode);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write %s\n", from), exi
-    t(99);
+		dprintf(STDERR_FILENO, "Error: Can't write %s\n", from), exit(99);
 	readme = read(file_from, count, 1024);
 	if (readme == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from), exit(98);
