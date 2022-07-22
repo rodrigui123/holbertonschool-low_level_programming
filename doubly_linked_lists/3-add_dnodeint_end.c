c33892ef3652750c0a433e546df09a9cf13b9737
Rodrigo <5093@holbertonschool.com>
2022-07-22 16:04:30 -0700

    t3add_dnodeint_end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - function that adds node at the end of list
+ * add_dnodeint_end - function that adds node at the end of list
  * lastdigit - last position to locate the new node
  * newnode - the node that will go at the end of list
  * @head : pointer of a pointer to list
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	listint_t *newnode;
 	listint_t *lastdigit = *head;
 
-	newnode = malloc(sizeof(listint_t));
+	newnode = malloc(sizeof(dlistint_t));
 	if (newnode)
 	{
 		if (*head == NULL)
