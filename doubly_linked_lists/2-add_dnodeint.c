d0c45382f65b6147e1d25784aa1d46166d3acc17
Rodrigo <5093@holbertonschool.com>
2022-07-22 15:36:17 -0700

    t2add_dnodeint

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
+	newnode->prev = NULL;
 	newnode->next = *head;
 	*head = newnode;
 
