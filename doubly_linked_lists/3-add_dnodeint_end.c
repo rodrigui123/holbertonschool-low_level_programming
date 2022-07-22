6bbd29cf2f026c575b29928c93c0f8e88fd49c24
Rodrigo <5093@holbertonschool.com>
2022-07-22 16:07:04 -0700

    t3add_dnodeint_end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,15 +10,17 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	listint_t *newnode;
-	listint_t *lastdigit = *head;
+	dlistint_t *newnode;
+	dlistint_t *lastdigit = *head;
 
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode)
 	{
 		if (*head == NULL)
+		{
 			newnode->prev = NULL;
 			*head = newnode;
+		}
 		else
 		{
 			while (lastdigit->next != NULL)
