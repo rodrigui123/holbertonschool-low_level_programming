5c9d2ea842606a98900260fa7425baf92be23a6a
Rodrigo <5093@holbertonschool.com>
2022-07-22 19:38:23 -0700

    t8delete_dnodeint

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,9 +1,44 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
+* delete_dnodeint_at_index - deletes node at index of a dlistint_t list
+* @head: pointer to head node
+* @index: index of node to delete
+* Return: Always EXIT_SUCCESS.
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *aux, *prevnode, *nextNode, *varnode;
+	unsigned int i;
+
+	aux = *head;
+	if (!aux)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		free(aux);
+		return (1);
+	}
+	for (i = 0; i < index; i++)
+	{
+		aux = aux->next;
+		if (!aux)
+			return (-1);
+	}
+	if (!aux->next)
+	{
+		varnode = aux;
+		varnode = aux->prev;
+		varnode->next = NULL;
+		free(aux);
+		return (1);
+	}
+i	prevnode = aux->prev;
+	nextnode = aux->next;
+	prevnode->next = nextnode;
+	nextnode->prev = prevnode;
+	free(aux);
+	return (1);
+}
