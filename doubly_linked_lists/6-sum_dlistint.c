58064b3caad5c0ab9dd501b744651587ba695059
Rodrigo <5093@holbertonschool.com>
2022-07-22 17:32:59 -0700

    t6sum_dlistint

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,20 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * sum_listint - func returns the sum of all the data list
+ * @head : pointer to first node in list
+ * Return: The sum
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	if (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum)
+}	
