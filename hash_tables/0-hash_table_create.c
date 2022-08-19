29345825cd6fca29076004e9ce8a523d203aeb9d
Rodrigo <5093@holbertonschool.com>
2022-08-19 13:14:30 -0700

    t0hash

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,12 +9,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	table = malloc(sizeof(hash_table_t *));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 	table->size = size;
 	return (table);
 }
