b58508901e77952318f148b8844dbaead12eca3a
Rodrigo <5093@holbertonschool.com>
2022-08-19 13:06:43 -0700

    t0hash

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_create - function that creates a hash table
+ * @size: size of the table
+ * Return: The table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -11,6 +11,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t *));
 	if (table == NULL)
+		return (NULL);
 	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
 	table->size = size;
+	return (table);
 }
