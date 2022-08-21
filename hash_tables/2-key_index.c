d05d812f9d2f81d2d8e68da1c788273bbefd4ad0
Rodrigo <5093@holbertonschool.com>
2022-08-21 15:13:38 -0700

    t2index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,9 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * key_index - 
+ * @key: 
+ * @size: size of key
+ * Return: position of key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	return (hash_djb2(key) % size);
+}
