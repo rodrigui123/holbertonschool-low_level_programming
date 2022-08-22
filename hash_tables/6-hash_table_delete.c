01b3ba82836d1e37bb2fdd0a3abbe3aee74037c6
Rodrigo <5093@holbertonschool.com>
2022-08-21 18:51:12 -0700

    t6delhash

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,9 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table to delete
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int index;
+	hash_node_t *before = NULL, *aux = NULL;
+
+	if (ht)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			before = ht->array[index];
+			while (before)
+			{
+				aux = before->next;
+				free(before->key);
+				free(before->value);
+				free(before);
+				before = aux;
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
