7d50991d02bd1950fb8c772b37b3f554d279dca5
Rodrigo <5093@holbertonschool.com>
2022-08-21 18:02:37 -0700

    t4hash

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,9 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table we are looking at
+ * @key: what we are searching
+ * Return: value associated with the element, or NULL
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *aux = NULL;
+
+	if (!ht || !key)
+		return (NULL);
+	index = key_index(key, ht->size);
+	aux = ht->array[index];
+
+	while (aux)
+	{
+		if (strcmp(aux->key, key) == 0)
+			return (aux->value);
+		aux = aux->next;
+	}
+	return (NULL);
+}
