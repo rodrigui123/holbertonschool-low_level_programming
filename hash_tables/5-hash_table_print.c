72d98da96820718d248cfe5c0c13b1498f7dc348
Rodrigo <5093@holbertonschool.com>
2022-08-21 18:39:20 -0700

    t5print

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,9 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table to print
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int index = 0, printcount = 0;
+	hash_node_t *aux = NULL;
+
+	if (!ht)
+		return;
+
+	putchar('{');
+	while (index < ht->size)
+	{
+		slot = ht->array[index];
+		while (aux)
+		{
+			if (printcount != 0)
+				printf(", ");
+			printf("'%s': '%s'", aux->key, aux->value);
+			aux = aux->next;
+			printcount++;
+		}
+		index++;
+	}
+	printf("}\n");
+}
