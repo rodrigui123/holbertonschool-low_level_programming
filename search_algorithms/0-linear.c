6047f4f1bffa20bd49454f8299cd4c135b7c84a6
Rodrigo <5093@holbertonschool.com>
2022-08-29 16:16:32 -0700

    t0checking

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,15 +4,15 @@
  * linear_search - searches for a value using the Linear search algorithm
  * @array: the array
  * @size: size of array
- * @value: value inside each position 
+ * @value: value inside each position
  * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
 	if (array == NULL)
-	for (i = 0; i < size; i++)
+	for (i; i < size; i++)
 	{
 		if (array[i] == value)
 		{
