ab858b00547f7d20d33da7aade58ba55299a6b6d
Rodrigo <5093@holbertonschool.com>
2022-08-29 16:12:25 -0700

    t0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value using the Linear search algorithm
+ * @array: the array
+ * @size: size of array
+ * @value: value inside each position 
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1)
+}
