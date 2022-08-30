25236aa36d92fbdc2a2383521a8e2cdc9bf38ecc
Rodrigo <5093@holbertonschool.com>
2022-08-29 17:31:16 -0700

    t1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -7,3 +7,18 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+
